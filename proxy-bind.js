56f487d825f2cdc7434a4d406c501d0251f64b28
muggy8 <muggy8@gmail.com>
2018-03-21 21:10:05 -0700

    some prelim stuff

--- proxy-bind.js
+++ proxy-bind.js
@@ -1,3 +1,38 @@
 var proxyBind = (function(){
-	
+	return function (template, model = {}){
+        var view = new DOMParser().parseFromString(template, "application/xml")
+
+        // dont ask why we aren't using recursion here
+        var addDataQueue = [view], currentTarget
+        while(currentTarget = addDataQueue.shift()){
+            Object.defineProperty(currentTarget, "data", createPropertyDefinition(true, true, {
+                get: function(){
+                    console.log("work in progress")
+                    return model
+                },
+                set: function(){
+                    console.log("work in progress")
+                }
+            }))
+            Array.prototype.push.apply(addDataQueue, currentTarget.childNodes)
+        }
+
+        return view
+    }
+
+    // because it's fun we're going to have JS hoist all of these through the return wheeeee
+    function createPropertyDefinition(configurable, enumerable, getSetOrVal){
+        var config = {
+            configurable: configurable,
+            enumerable: enumerable
+        }
+        if (getSetOrVal.get){
+            config.get = getSetOrVal.get
+            getSetOrVal.set && (config.set = getSetOrVal.set)
+        }
+        else {
+            config.value = getSetOrVal
+        }
+        return config
+    }
 })()
