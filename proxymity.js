5f86b7719ba48d68bd38da43b3b6ab7646867eb3
muggy8 <muggy8@gmail.com>
2018-03-25 00:46:02 -0700

    started to implement the basis of creating the proxy data

--- proxymity.js
+++ proxymity.js
@@ -1,11 +1,26 @@
 proxymity = (function(saveEval){
-	return function(template, html){}
+	return function(template, dataModel = {}){
+		var events = new subscribable();
+		return proxyObj({}, events, "data")
+	}
 
 	function proxyObj(obj, eventInstance, eventNamespace){
 		if (Array.isArray(obj)){
 			return proxyArray(obj, eventInstance, eventNamespace)
 		}
-		return new Proxy(obj, )
+		else if (typeof obj ===  "Object" ){
+			return new Proxy(obj, {
+				get: function(target, property){
+					if (target.hasOwnProperty(property)){
+						return target[property]
+					}
+					else {
+						target[property] = proxyObj({}, eventInstance, eventNamespace + "." + property)
+						return target[property]
+					}
+				}
+			})
+		}
 	}
 
 	function proxyArray(arr, eventInstance, eventNamespace){
