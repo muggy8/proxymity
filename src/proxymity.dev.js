c9a6fb11daeeedd9cf2ddda0227b8d75a91ca033
muggy8 <muggy8@gmail.com>
2018-04-09 12:44:22 -0700

    oops typo

--- src/proxymity.dev.js
+++ src/proxymity.dev.js
@@ -8,9 +8,9 @@ var proxymity = (function(safeEval){
 		var events = new subscribable()
 
 		// we only want to render the view at most twice per primary update cycle. an update cycle is when something somewhere in the main data object is modified from either the UI or the user code, either way if we have any code that modifies the main data on rendering through the {{}} syntax (eg: {{this.data.filteredArray = this.data.array.filter(item=>item.caninclude)}}), the second render will allow that to show up, however because the re-render action is triggered from within a render cycle, this will lead to an infinite render loop if left unchecked which isn't really good for battery or UX so we want to limit the render cycle to forcefully complete after 2 renders which is rather reasonable. The 2 following listeners will accomplish this by emiting events at the start and end of the events that will trigger the render cycle and therefore must be seperated by the main initiation code. the proxy obj will check the last emited rendering event before emitting events that would trigger UI re-renders
-		events.watch("asyncstart"){
+		events.watch("asyncstart", function(){
 			events.emit("rendering", {value: true})
-		}
+		})
 
 		events.async("set:")
 		var proxied = proxyObj(initialData, events)
@@ -26,9 +26,9 @@ var proxymity = (function(safeEval){
 			}
 		})
 
-		events.watch("asyncend"){
+		events.watch("asyncend", function(){
 			events.emit("rendering", {value: false})
-		}
+		})
 		return ui
 	}
 })(function(script, contextVars = {}){
