078c0fcdd7357724309dba4cdba5b8caf7fcc0bb
muggy8 <muggy8@gmail.com>
2018-04-25 09:11:48 -0700

    began refactoring path observation out of proxy ui

--- src/proxymity-observe.js
+++ src/proxymity-observe.js
@@ -1,3 +1,32 @@
-observe(model, events, targetFinder, callbackSet){
+function observe(events, targetFinder, callbackSet, stuffToUnWatch = []){
+    targetFinder()
+    var targetId = events.last("get").value
+    var targetId = events.last("get").value
 
+    if (isFunction(callbackSet)){
+        var callback = callbackSet
+        
+        callbackSet = [
+            {
+                to: "del",
+                fn: callback
+            },
+            {
+                to: "set",
+                fn: callback
+            }
+        ]
+    }
+
+    forEach(callbackSet, function(callback){
+        var type = callback.to + ":" + targetId
+        events.watch(type, callback.fn)
+        var lastEvent = events.last(type)
+        if (typeof lastEvent !== 'undefined'){
+            callback.fn(lastEvent)
+        }
+    })
+    
+
+    console.log(targetId)
 }
