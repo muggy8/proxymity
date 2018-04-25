0f0e6addf77d62cad94668e1440266e4c1fd5214
muggy8 <muggy8@gmail.com>
2018-04-25 12:20:38 -0700

    this kinda seems to work for permanent watching but other problems are found

--- src/proxymity-observe.js
+++ src/proxymity-observe.js
@@ -1,7 +1,6 @@
 function observe(events, targetFinder, callbackSet, stuffToUnWatch = []){
     targetFinder()
     var targetId = events.last("get").value
-    var targetId = events.last("get").value
 
     if (isFunction(callbackSet)){
         var callback = callbackSet
@@ -20,13 +19,29 @@ function observe(events, targetFinder, callbackSet, stuffToUnWatch = []){
 
     forEach(callbackSet, function(callback){
         var type = callback.to + ":" + targetId
-        events.watch(type, callback.fn)
+        stuffToUnWatch.push(events.watch(type, callback.fn))
         var lastEvent = events.last(type)
         if (typeof lastEvent !== 'undefined'){
             callback.fn(lastEvent)
         }
     })
-    
+
+    var clearWatchers = function(){
+        forEach(stuffToUnWatch, function(fn){
+            fn()
+        })
+        stuffToUnWatch.length = 0
+    }
+
+    var onReMap = function(){
+        clearWatchers()
+        observe(events, targetFinder, callbackSet, stuffToUnWatch)
+    }
+
+    stuffToUnWatch.push(events.watch("remap:" + targetId, onReMap))
+    stuffToUnWatch.push(events.watch("del:" + targetId, onReMap))
 
     console.log(targetId)
+
+    return clearWatchers
 }
