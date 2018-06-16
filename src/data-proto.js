d4a766d9cd768e9581c5ae57a1a99bbd4a36e74c
muggy8 <muggy8@gmail.com>
2018-06-15 17:14:29 -0700

    can now create replacements for array and objects

--- src/data-proto.js
+++ src/data-proto.js
@@ -12,28 +12,46 @@ function keyCopier(context, keySource){
         })
     }
 }
-function DataProto(dataTypeProto){
-    var context = {}
+var proxyTraps = {
+    get: function(target, prop, calledOn) {
+        Reflect.get(target, prop, calledOn)
+        console.log("get")
+        console.log.apply(console, arguments)
+    },
+    set: function(target, prop, value, calledOn){
+        Reflect.set(target, prop, value, calledOn)
+        console.log("set")
+        console.log.apply(console, arguments)
+        return true
+    }
+}
+function proxify(originalProto){
+    var replacementProto = {}
 
-    var getKeysFrom = dataTypeProto
+    // first we copy everything over to the new proto object that will sit everywhere so this object will catch any calls to the existing object and all parent objects to bypass and avoid the need to reach the proxy when using anything
+    var getKeysFrom = originalProto
     while (getKeysFrom){
-        forEach(Object.getOwnPropertyNames(getKeysFrom), keyCopier(context, getKeysFrom))
+        var copyIntoReplacement = keyCopier(replacementProto, getKeysFrom)
+        forEach(Object.getOwnPropertyNames(getKeysFrom), copyIntoReplacement)
         getKeysFrom = Object.getPrototypeOf(getKeysFrom)
     }
 
-    Object.setPrototypeOf(context, Object.getPrototypeOf(this))
-    return context
+    // afterwards we want to set the prototype of the replacement prototype object to a proxy so when we set any new properties, we can catch that and create a getter/setter combo on the main object.
+    // we want to still retain the original proto in the proxy because in case something changes on the prototype because someone is loading in a utils library (for example, a library that adds methos to modifies array.prototype) after we are done, we are able to also reflect that and stick it into the proto layer
+    Object.setPrototypeOf(replacementProto, new Proxy(originalProto, proxyTraps))
+    return replacementProto
 }
-DataProto.prototype = new Proxy({}, {
-    get: function(target, prop, calledOn) {
-        console.log.apply(console, arguments)
-        Reflect.get(target, prop, calledOn)
-    },
-    set: function(target, prop, value, calledOn){
-        console.log.apply(console, arguments)
-        Reflect.set(target, prop, value, calledOn)
-    }
-})
 
-var protoArrayProxy = new DataProto(Array.prototype)
-var protoObjectProxy = new DataProto(Object.prototype)
+var protoArrayProxy = new proxify(Array.prototype)
+var protoObjectProxy = new proxify(Object.prototype)
+
+function proxyArray(normalArray){
+    var arr = Object.setPrototypeOf([], protoArrayProxy)
+    // moar logix here
+    return arr
+}
+function proxyObject(regularObj){
+    var obj = Object.create(protoObjectProxy)
+    // some magic logic here
+    return obj
+}
