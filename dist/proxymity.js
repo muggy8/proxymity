fb85f8585ace23774ee5973ff7bd5e697217cac7
muggy8 <muggy8@gmail.com>
2018-08-09 15:10:56 -0700

    better default protection with less new empty arrays

--- dist/proxymity.js
+++ dist/proxymity.js
@@ -6,7 +6,7 @@ function arrayFrom(arrayLike){ // incase we are running in a not so new browser 
 }
 
 function forEach(arrayLike, callback){
-	return Array.prototype.forEach.call(arrayLike || [], callback)
+	return arrayLike && isNumber(arrayLike.length) && Array.prototype.forEach.call(arrayLike, callback)
 }
 
 function isFunction(val){
