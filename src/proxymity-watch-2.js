0391f514fc91bc5e62fbdb2cee8f3ba0a440ad3b
muggy8 <muggy8@gmail.com>
2019-07-27 01:53:18 -0700

    adding more implementation to the create watchable props function

--- src/proxymity-watch-2.js
+++ src/proxymity-watch-2.js
@@ -22,17 +22,35 @@ function createWatchableProp(obj, prop, value = {}, config = {}){
 	var descriptor
 	overrideArrayFunctions(value)
 
+	// the scope of this function is where the value of the properties are stored. in here we can also watch for state changes via the getters and setters allowing us to update the view when the view updates as we can detect it with this pair of getters and setters.
 	Object.defineProperty(obj, prop, descriptor = {
 		enumerable: hasProp(config, "enumerable") ? config.enumerable : true,
 		configurable: hasProp(config, "configurable") ? config.configurable : true,
 		get: function(onChangeCallack, onDeleteCallback){
-			if (onChangeCallack && onDeleteCallback){
+			// the getter function serves double duty. since the getter function is declared here, it has access to the scope of the parent function and also leaves no major residue or attack surface for people to get into the internals of this library, it's unlikely that someone is able to gain access to many of the secrets of the internals of the library at run time through this function. However because this function can be called normally outside of just using assignments, we are able to have this function serve double duty as the entry point to add callbacks to the watch method as well as being the getter under normal query and assignment operations.
 
+			if (onChangeCallack && onDeleteCallback){
+				var link = callbackSet.find(function(item){
+					return item.set === onChangeCallack && item.del === onDeleteCallback
+				})
+
+				!link && (link = callbackSet.push({
+					set: onChangeCallack,
+					del: onDeleteCallback
+				}))
+
+				return link.drop
 			}
 			return value
 		},
 		set: function(newValue){
+			if (typeof newValue === "undefined"){
+				// attempting to delete this prop we should call the del callback of all watchers attached to this item
 
+			}
+			else{
+				// updated the stuff lets call all the set callbacks
+			}
 		}
 	})
 
@@ -93,9 +111,13 @@ function LinkedItem(payload, belongsTo){
 	}
 }
 function dropLinkedItem(item, belongsTo){
-	console.log(123, item, belongsTo)
-	item.prev && (item.prev.next = item.next)
-	item.next && (item.next.prev = item.prev)
+	var hasChanged = false
+	item.prev && item.prev.next === item && ((item.prev.next = item.next) + (hasChanged = true))
+	item.next && item.next.prev === item && ((item.next.prev = item.prev) + (hasChanged = true))
+
+	if (!hasChanged){
+		return
+	}
 	belongsTo.first === item && (belongsTo.first = item.next)
 	belongsTo.last === item && (belongsTo.last = item.prev)
 	belongsTo.length--
